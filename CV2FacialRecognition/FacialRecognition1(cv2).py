4a79811a2575427a05abdca82acbb40391d6ce45
Christopher Coram <ultraartist15@gmail.com>
2019-03-02 13:27:36 -0500

    Add files via upload

---
+++ CV2FacialRecognition/FacialRecognition1(cv2).py
@@ -0,0 +1,21 @@
+# START...cv2 intigrated jpg facial comparison
+# Imports for script
+import cv2
+import numpy as np
+
+# Variables 
+cap = cv2.VideoCapture(0)
+
+# While true, imshow will stay open in loop creating constant frame operation
+while(True):
+   # Capture frame-by-frame
+   ret, frame = cap.read()
+
+   # Display the resulting frame
+   cv2.imshow('frame',frame)
+   if cv2.waitKey(20) & 0xFF == ord('q'):
+     break
+
+# Functions will activate to complete script actions
+cap.release()
+cv2.destroyAllWindows()
