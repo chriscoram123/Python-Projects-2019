af91c3ed484105b95f9ed05e7b58199fb8bf82b3
Christopher Coram <ultraartist15@gmail.com>
2019-05-09 02:47:23 -0400

    Add files via upload

---
+++ MachineLearning/Find Points On Number Line.py
@@ -0,0 +1,15 @@
+from sklearn import tree
+# Label out the main plot points on hypothetical number line using machine learning/while loop
+numberLineFeatures = [[20,1],[10,0],[0,3],[10,4],[20,2]]
+# mainPointLabels operate as open and closed circles on number line
+# 1 and 0 = open circles
+# 3,2 and 4 = closed circles
+mainPointLabels = [1,0,2,4,3]
+N = input("Plot Points")
+def findPoints():
+	print("Please insert Plot Points to begin.")
+	while N == "Plot Points"
+	 clf = tree.DecisionTreeClassifier()
+     clf = clf.fit(numberLineFeatures, mainPointLabels)
+     print(clf.predict([]))
+     break
