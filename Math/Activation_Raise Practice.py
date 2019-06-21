c1efee96112dd6f6f17cc19a75033d1b47a1dd2e
Christopher Coram <ultraartist15@gmail.com>
2019-06-21 07:13:34 -0400

    Add files via upload

---
+++ Math/Activation_Raise Practice.py
@@ -0,0 +1,26 @@
+# Physics Formula Application
+"""
+@ Christopher Sean Coram
+6/21/2019 4:54 AM
+"""
+# Class of data
+Class Formula():
+    x = input("Execute")
+    xi = 20
+    xf = 80
+    Displacement = xf - xi
+    def FormulaExecution(self):
+    if x == "Execute"
+    print(Displacement)
+    else:
+    raise Exception("Results should be ...".format)
+   
+object_x = Formula()
+
+# User input to activate class
+Y = input("Activate")
+def userInput():
+  if Y == "Activate":
+   print(object_x.FormulaExecution())
+  else:
+ raise Exception("Results should be ...".format)
