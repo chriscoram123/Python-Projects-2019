c70b94228c00dc844c2a593d361e80c6e294cfd1
Christopher Coram <ultraartist15@gmail.com>
2019-10-05 21:51:51 -0400

    Add files via upload

---
+++ Organizing Data/labex5.py
@@ -0,0 +1,28 @@
+a = 5
+b = 3
+c = 10
+d = 4
+
+if a * b > c + d:
+    print(a * 5)
+else:
+    print(c * 5)
+
+
+if a > b and c > d:
+    print('True')
+else:
+    print('Not True')
+
+
+if a < b or c > d and d > b:
+    print('Yes')
+else:
+    print('No')
+
+
+if a * b - c > b + d or c - b < a + d:
+    print('First')
+else:
+    print('Second')
+
